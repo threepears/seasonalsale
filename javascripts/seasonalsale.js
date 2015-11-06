15505b7a76064ad1a43c5f6de0c4a753c5535e26
NathanBaldwin <you@example.com>
2015-11-06 16:09:59 -0600

    added ajax calls

--- javascripts/seasonalsale.js
+++ javascripts/seasonalsale.js
@@ -1,14 +1,18 @@
-define(["jquery"],
-	function($) {
+define(["jquery", "getProductInfo"],
+	function($, getProductInfo) {
 
-		return {
-			productInfo: function() {
-				require([ ])
-			}
+		var spring = $("#spring");
+		var summer = $("#summer");
+		var fall = $("#fall");
+		var winter = $("#winter");
+		var content = $("#content");
 
+		function populateDom(data) {
+			require(["hbs!../templates/product"], function(dataTemplate) {
+				content.append(dataTemplate(data));
+			});
 		}
 
-
-
+		getProductInfo.productInfo(populateDom);
 
 });
